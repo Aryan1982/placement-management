0819b50423ac9b4e251bbdfe5337d80ccf48f5a0
Aryan Thakor <62349184+Aryan1982@users.noreply.github.com>
2024-07-21 07:28:38 +0000

    added applied flag to company for student login

--- src/app/Pages/Company/companyProfile/companyProfile.component.ts
+++ src/app/Pages/Company/companyProfile/companyProfile.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { ActivatedRoute, Data } from '@angular/router';
+import { RouterTestingHarness } from '@angular/router/testing';
 import { CommonService } from 'src/app/services/common.service';
 import { CommonApiService } from 'src/app/services/commonApi.service';
 
@@ -11,50 +12,58 @@ import { CommonApiService } from 'src/app/services/commonApi.service';
 })
 export class CompanyProfile {
   companyData: any;
-  companyId: string | undefined;
+  companyId: any;
 
   studentDetails: FormGroup | undefined;
   studentId: any;
+  userRecordString: any;
+  hasApplied: boolean = false;
 
   constructor(
     public studentApplyDetailsForm: FormBuilder,
     public commonService: CommonService,
-    private commonApiService: CommonApiService,
+    public commonApiService: CommonApiService,
     private route: ActivatedRoute
   ) {}
 
+  ngOnInit(): void {
+    this.userRecordString = localStorage.getItem('userRecord');
+    this.companyId = this.route.snapshot.paramMap.get('id')!;
+    this.studentId = JSON.parse(this.userRecordString).studentId;
+    this.isStudentApplied();
+    if (this.companyId) {
+      this.commonApiService
+        .getRequest(`/api/collections/CompanyDetails/records/${this.companyId}`)
+        .subscribe((res: any) => {
+          this.companyData = res;
+        });
+    }
+  }
+
+ async isStudentApplied() {
+      this.hasApplied = await this.commonApiService.hasStudentAppliedToCompany(this.studentId, this.companyId);
+  }
+
   applyToCompany() {
-    const userRecordString = localStorage.getItem('userRecord');
-    if (!userRecordString) {
+    if (!this.userRecordString) {
       return;
     }
-    this.studentId = JSON.parse(userRecordString).studentId;
+
     this.studentDetails = this.studentApplyDetailsForm.group({
       student_id: [this.studentId],
-      company_id: [this.companyId ?? 'w4m20sdwz0v6adr'],
+      company_id: [this.companyId],
     });
-
     this.commonApiService
       .postRequest(
-        `/api/collections/Student_applied/records`,
+        `/api/collections/Applications/records`,
         this.studentDetails.value
       )
       .subscribe((res: any) => {
-        console.log(
-          `student with companyId ${this.companyId} applied successfully`
-        );
-        console.log('------------------------------------------------');
-        console.log(res);
+        alert(`Applied successfully`);
       });
   }
 
-  ngOnInit(): void {
-    this.companyId = this.route.snapshot.paramMap.get('id')!;
-    this.commonApiService
-      .getRequest(`/api/collections/CompanyDetails/records/${this.companyId}`)
-      .subscribe((res: any) => {
-        this.companyData = res;
-        console.log(res);
-      });
+  alreadyApplied() {
+    alert('You have already applied to this company');
   }
 }
