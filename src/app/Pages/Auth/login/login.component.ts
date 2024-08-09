7648c7269f1b341856cc878eebdeebf74fc99daf
Aryan Thakor <62349184+Aryan1982@users.noreply.github.com>
2024-08-09 18:37:24 +0000

    route to company list on student login

--- src/app/Pages/Auth/login/login.component.ts
+++ src/app/Pages/Auth/login/login.component.ts
@@ -6,7 +6,7 @@ import { CommonApiService } from 'src/app/services/commonApi.service';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
   loginForm: FormGroup;
@@ -23,33 +23,38 @@ export class LoginComponent {
   }
 
   userLogin() {
-    this.commonApiService.postRequest("api/collections/users/auth-with-password", this.loginForm.value)
+    this.commonApiService
+      .postRequest(
+        'api/collections/users/auth-with-password',
+        this.loginForm.value
+      )
       .subscribe({
         next: (res: any) => {
-          if (res.record.role.toLowerCase() == "student") {
-
-            this.commonApiService.getRequest(`/api/collections/Student/records?filter=(email="${res.record.email}")`).subscribe((student: any) => {
-              console.log(student.items[0].id)
-              res.record.studentId = student.items[0].id
-              localStorage.setItem('userRecord', JSON.stringify(res.record))
-              return
-            })
-            this.router.navigateByUrl("/companylist");
+          if (res.record.role.toLowerCase() == 'student') {
+            this.commonApiService
+              .getRequest(
+                `/api/collections/Student/records?filter=(email="${res.record.email}")`
+              )
+              .subscribe((student: any) => {
+                console.log(student.items[0].id);
+                res.record.studentId = student.items[0].id;
+                localStorage.setItem('userRecord', JSON.stringify(res.record));
+                this.router.navigateByUrl('/companylist');
+                return;
+              });
           }
-          localStorage.setItem('userRecord', JSON.stringify(res.record))
-          this.router.navigateByUrl("/studentlist");
+          localStorage.setItem('userRecord', JSON.stringify(res.record));
+          this.router.navigateByUrl('/studentlist');
         },
         error: (error) => {
-          console.error("An error occurred:", error);
+          console.error('An error occurred:', error);
           // Handle error here, for example:
           if (error.status === 400) {
-            alert("Unauthorized. Please check your credentials.");
+            alert('Unauthorized. Please check your credentials.');
           } else {
-            alert("An error occurred. Please try again later.");
+            alert('An error occurred. Please try again later.');
           }
-        }
+        },
       });
   }
-
-
 }
